e6c1c3f8da2ddeca9ed8237e0818ec3c21de7401
aadroozeProgrammer <aadroozeprogrammer@gmail.com>
2026-02-14 16:29:51 -0500

    2/14/2026 commit

---
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeoController : MonoBehaviour
+{
+    string. variableOne = "Hello";
+    {
+
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        string.variableTwo = "World";
+        Debug.Log("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+Debug.Log(variableOne + variableTwo);
