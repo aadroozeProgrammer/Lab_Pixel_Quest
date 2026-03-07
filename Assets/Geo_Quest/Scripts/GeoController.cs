dbc43f31da9c037910c3fc2d09aea0bcb42ced44
aadroozeProgrammer <aadroozeprogrammer@gmail.com>
2026-03-07 16:35:15 -0500

    3/7/2026

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -1,23 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GeoController : MonoBehaviour
 {
-    string variableOne = "Hello";
-  
+    private Rigidbody2D rb;
+    public int speed = 5;
+    public string nextLevel = "Scene_2";
+
     // Start is called before the first frame update
     void Start()
     {
-        string variableTwo = "World";
-        Debug.Log("Hello World");
-        Debug.Log(variableOne + variableTwo);
+        rb = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float xInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
     }
-}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.tag)
+        {
+            case "Death":
+                {
+                    string thisLevel = SceneManager.GetActiveScene().name;
+                    SceneManager.LoadScene(thisLevel);
+                    break;
+                }
+            case "Finish":
+                {
+                    SceneManager.LoadScene(nextLevel);
+                    break;
+                }
+        }
 
+    }
+
+}
