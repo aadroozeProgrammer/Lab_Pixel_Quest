1bc110c3a39620a888e55d5fb553c8099c9234b8
kaikaisakamoto1115-gif <aadroozeprogrammer@gmail.com>
2026-03-07 18:58:57 -0500

    3/7/2026

--- Assets/Hands-On Homework #2/Scripts/HW2PlayerShoot.cs
+++ Assets/Hands-On Homework #2/Scripts/HW2PlayerShoot.cs
@@ -2,5 +2,33 @@ using UnityEngine;
 
 public class HW2PlayerShoot : MonoBehaviour
 {
+    public GameObject preFab;
+    public Transform bulletTrash;
+    public Transform bulletSpawn;
 
+    private const float Timer = 0.5f;
+    private float _currentTime = 0.5f;
+    private bool _canShoot = true;
+
+    private void Update()
+    {
+        if (!_canShoot)
+        {
+            _currentTime -= Time.deltaTime;
+
+            if (_currentTime < 0)
+            {
+
+            }
+
+
+
+            if (Input.GetKeyDown(KeyCode.Mouse0) && _canShoot)
+            {
+                GameObject bullet = Instantiate(preFab, bulletSpawn.position, Quaternion.identity);
+
+                bullet.transform.SetParent(bulletTrash);
+            }
+        }
+    }
 }
